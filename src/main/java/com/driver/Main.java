b229e4b0a59b3e4820901bb471ab2d6c2e074b7e
PriyadarshaniD <dhandepriyadarshani15@gmail.com>
2024-07-03 12:30:14 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly ob = new RWOnly();
+      //ob.name= "Priya";
+      //System.out.println(ob.name);
+      ob.setName("Priya");
+      System.out.println(ob.getName());
+  }
 }
